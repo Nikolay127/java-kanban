f851a4b4ad54512b41f91122668e059b3a5cc335
Nikolay Abakumov <abackumov.nikolai@yandex.ru>
2024-05-08 19:22:57 +0300

    Внести корректировки согласно ревью по ТЗ-7

--- src/ru/yandex/javacource/abakumov/schedule/exceptions/ManagerSaveException.java
+++ src/ru/yandex/javacource/abakumov/schedule/exceptions/ManagerSaveException.java
@@ -1,6 +1,6 @@
 package ru.yandex.javacource.abakumov.schedule.exceptions;
 
-public class ManagerSaveException extends RuntimeException{
+public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException(String message, Exception e) {
         super(message, e);
